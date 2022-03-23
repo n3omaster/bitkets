6aebdbba8b162c27ebde2bce02c2058abdd9cc5f
Erich <ecruz@bachecubano.com>
2022-03-22 22:06:11 -0400

    503

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,28 @@
+<x-app-layout>
+    <section id="home" class="ud-relative ud-z-10 ud-overflow-hidden ud-bg-cover ud-bg-top ud-bg-no-repeat ud-pt-[150px] ud-pb-24" style="background-image: url('./images/hero/common-bg.jpg')">
+        <div class="ud-absolute ud-left-0 ud-top-0 ud--z-10 ud-h-full ud-w-full" style="background: linear-gradient(180deg,rgba(20, 20, 32, 0.65) 0%,#141420 100%);"></div>
+        <div class="ud-container">
+            <div class="ud--mx-4 ud-flex ud-flex-wrap ud-items-center">
+                <div class="ud-w-full ud-px-4 lg:ud-w-1/2">
+                    <div class="ud-mb-12 ud-max-w-[570px] lg:ud-mb-0">
+                        <h1 class="ud-mb-4 ud-text-[40px] ud-font-bold ud-leading-tight ud-text-white md:ud-text-[50px] lg:ud-text-[40px] xl:ud-text-[46px] 2xl:ud-text-[50px] sm:text-[46px]">
+                            Volvemos enseguida ⚡️
+                        </h1>
+                        <p class="ud-mb-8 ud-text-lg ud-font-medium ud-leading-relaxed ud-text-body-color md:ud-pr-14">
+                            Estamos realizando algunos ajustes para mejorar la experiencia y funcionalidad de la plataforma.
+                        </p>
+                        <div class="flex flex-wrap items-center">
+                            👍
+                        </div>
+                    </div>
+                </div>
+
+                <div class="ud-w-full ud-px-4 lg:ud-w-1/2">
+                    <div class="ud-text-center">
+                        <img src="images/hero/hero-image.svg" alt="hero image" class="ud-mx-auto ud-max-w-full" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</x-app-layout>
