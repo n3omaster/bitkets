4f855a9c8b03ac4bfd37907b88be90becd9061de
Erich <ecruz@bachecubano.com>
2022-03-24 22:36:44 -0400

    remove instagram logo

--- resources/views/livewire/buy-ticket.blade.php
+++ resources/views/livewire/buy-ticket.blade.php
@@ -28,7 +28,7 @@
     </div>
     @elseif($completed == true)
     <div class="ud-flex ud-flex-col">
-        <div class="ud-p-4 md:w-1/2 ud-w-full">
+        <div class="ud-w-full">
             <div class="ud-h-full ud-bg-gray-100 ud-p-8 ud-rounded">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="ud-block ud-w-5 ud-h-5 ud-text-gray-400 ud-mb-4" viewBox="0 0 975.036 975.036">
                     <path d="M925.036 57.197h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.399 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l36 76c11.6 24.399 40.3 35.1 65.1 24.399 66.2-28.6 122.101-64.8 167.7-108.8 55.601-53.7 93.7-114.3 114.3-181.9 20.601-67.6 30.9-159.8 30.9-276.8v-239c0-27.599-22.401-50-50-50zM106.036 913.497c65.4-28.5 121-64.699 166.9-108.6 56.1-53.7 94.4-114.1 115-181.2 20.6-67.1 30.899-159.6 30.899-277.5v-239c0-27.6-22.399-50-50-50h-304c-27.6 0-50 22.4-50 50v304c0 27.601 22.4 50 50 50h145.5c-1.9 79.601-20.4 143.3-55.4 191.2-27.6 37.8-69.4 69.1-125.3 93.8-25.7 11.3-36.8 41.7-24.8 67.101l35.9 75.8c11.601 24.399 40.501 35.2 65.301 24.399z"></path>
@@ -37,7 +37,7 @@
                 <p class="ud-leading-relaxed ud-mb-6">Su ticket de participación:</p>
                 <h1 class="ud-font-bold ud-text-3xl ud-text-center">{{ $ticket }}</h1>
                 <a class="ud-inline-flex ud-items-center">
-                    <img alt="testimonial" src="https://bitkets.nyc3.digitaloceanspaces.com/brands/vistar.png" class="ud-w-12 ud-flex-shrink-0 ud-object-cover ud-object-center">
+                    <img alt="{{ $event->owner->name }}" src="https://bitkets.nyc3.digitaloceanspaces.com/brands/vistar.png" class="ud-w-12 ud-flex-shrink-0 ud-object-cover ud-object-center">
                     <span class="ud-flex-grow ud-flex ud-flex-col ud-pl-4">
                         <span class="ud-title-font ud-font-medium ud-text-gray-900">{{ $event->owner->name }}</span>
                         <span class="ud-text-gray-500 ud-text-sm">Organizador</span>
