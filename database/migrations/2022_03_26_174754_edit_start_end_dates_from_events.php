81bb13dbf1e9b30c2f8764e501c1ad90e30b02d6
Erich <ecruz@bachecubano.com>
2022-03-26 16:07:04 -0400

    DB Migration: Modify start and ending columns for eventas table

---
+++ database/migrations/2022_03_26_174754_edit_start_end_dates_from_events.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            // Modify start and ending columns for date_time objects
+            $table->dateTime('start')->change();
+            $table->dateTime('end')->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('events', function (Blueprint $table) {
+            //
+        });
+    }
+};
